1a63140ff04127ea6e840893249678db0d22d0d8
Austin Higgins <austinjhiggins@pm.me>
2024-08-09 14:14:54 -0230

    Setup steamAPI and sucessfully made an API call and returned it. Hid the API key.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { useState, useEffect } from 'react'
+
+const API_KEY = import.meta.env.VITE_STEAM_API_KEY;
+const URL = 'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=230410&key=' + API_KEY + '&steamid=76561198119786249'
 
 console.log('testing')
+
+fetch(URL).then((res) => res.json()).then((data) => {
+  console.log(data)
+})
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+return (
+  <div className='test'></div>
+)
   </StrictMode>,
 )
